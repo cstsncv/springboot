eab78b17656e9b073ce0a211bcda0265d700cb8f
fxl <cstsncv@sina.com>
2021-04-12 15:38:46 +0800

    SpringBoot CRUD

--- springboot-web-restfulcrud/src/main/java/com/csts/restfulcrud/controller/EmployeeController.java
+++ springboot-web-restfulcrud/src/main/java/com/csts/restfulcrud/controller/EmployeeController.java
@@ -7,8 +7,7 @@ import com.csts.restfulcrud.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -41,14 +40,43 @@ public class EmployeeController {
         return "emp/add";
     }
 
-
+    // 员工添加
     @PostMapping("/emp")
     public String addEmp(Employee employee){
 
-        System.out.println(employee);
+        System.out.println("add Employee: " + employee);
         employeeDao.save(employee);
         // redirect: 重定向
         // forward: 转发
+        return "redirect:/emps";
+    }
+
+    // 修改, 查出当前员工, 在页面回显
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id, Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp", employee);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        //回到修改页面(add页面是一个修改添加二合一页面)
+        return "emp/add";
+    }
+
+    // 员工修改
+    @PutMapping("/emp")
+    public String editEmp(Employee employee){
+        System.out.println("edit Employee: " + employee);
+        employeeDao.save(employee);
+        // redirect: 重定向
+        // forward: 转发
+        return "redirect:/emps";
+    }
+
+    //员工删除
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmp(@PathVariable("id") Integer id){
+        System.out.println("delete EmployeeId: " + id);
+        employeeDao.delete(id);
         return "redirect:/emps";
     }
 }
